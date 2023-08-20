3551eb161f4c757488010900b7abb3c2d223f3e3
CCA2878 <1054331608@qq.com>
2023-08-20 23:26:42 +0800

    Add account checking when login

--- assets/js/index.js
+++ assets/js/index.js
@@ -28,7 +28,20 @@ function register() {
 // 账号登录 Start
 function login() {
     document.getElementById('card-index').style.pointerEvents = 'none';
-    window.location.href = "/daily/config.html?account=" + $("#account").val();
+    let account = $("#account").val()
+    $.ajax({
+        url: "/daily/api/login?account=" + account,
+        type: "get",
+        processData: false,
+        success: function (ret) {
+            window.location.href = "/daily/config.html?account=" + account;
+        },
+        error: function (ret) {
+            alert("账号无效或不存在，第一次使用请先注册。\n如需帮助，请联系管理员。\n" + ret.responseText);
+            document.getElementById('card-index').style.pointerEvents = 'auto';
+        }
+    });
+    // window.location.href = "/daily/config.html?account=" + $("#account").val();
 }
 // 账号登录 End
 document.addEventListener('keydown', function(event) {
